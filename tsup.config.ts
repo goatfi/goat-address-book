63d42020accae35cf0739dd8667727c108cddcdc
reikodoteth <149171651+reikodoteth@users.noreply.github.com>
2024-10-24 12:22:40 +0200

    Fix Module

---
+++ tsup.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'tsup';
+import { esbuildPluginFilePathExtensions } from 'esbuild-plugin-file-path-extensions';
+
+// https://github.com/egoist/tsup/issues/953
+export default defineConfig([
+    {
+        format: ['cjs', 'esm'],
+        entry: ['src/**/*.ts'],
+        outDir: './dist',
+        bundle: true,
+        sourcemap: true,
+        // clean: true,
+        splitting: false,
+        dts: true,
+        esbuildPlugins: [esbuildPluginFilePathExtensions({ cjsExtension: 'js' })],
+    }
+]);
