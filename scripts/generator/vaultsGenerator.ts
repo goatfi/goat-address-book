a8fde66499da24cecf5add7bc74df582ed5efe85
reikodoteth <149171651+reikodoteth@users.noreply.github.com>
2024-10-28 22:59:18 +0100

    Fix vault generation

--- scripts/generator/vaultsGenerator.ts
+++ scripts/generator/vaultsGenerator.ts
@@ -3,13 +3,13 @@ import type { NetworkVaults } from '../configs/types';
 
 export function generateVaultAddresses(config: NetworkVaults) {
     const name = `Vaults${config.name}`;
-
+    const vaultsArray = config.vaults.map((v) => {return `'${v}'`});
     if (config.vaults.length > 0) {
         const data = `import type { Hex } from 'viem'; 
         export const name = "${config.name}";
         export const chainId = ${config.chainId};
         export const vaults: Hex[] = [
-            "${config.vaults}"
+            ${vaultsArray}
         ];`;
         fs.writeFileSync(`./src/ts/${name}.ts`, data, 'utf-8');
         return {
